ce44aacd6d50081cb927513df537a12c6a02f1f1
adrien <adrien@ghost.seraum.local>
2017-09-01 17:29:08 +0200

    Updated getting started

--- code/start/gs.sh
+++ code/start/gs.sh
@@ -5,4 +5,7 @@ apt-get update && apt-get install curl
 curl -sL https://deb.nodesource.com/setup_8.x | bash -
 
 echo 'TECHIO> terminal'
+
+echo 'TECHIO> redirect-streams --input "Terminal" "Standard Output"'
+
 sleep 2m
