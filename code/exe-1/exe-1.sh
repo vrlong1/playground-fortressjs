073149229b56362fa4866a6e180b7749682b4e56
adrien <adrien@ghost.seraum.local>
2017-09-05 15:44:34 +0200

    Updated

--- code/exe-1/exe-1.sh
+++ code/exe-1/exe-1.sh
@@ -5,3 +5,13 @@ apt-get update
 apt-get install -y git nodejs
 
 echo 'TECHIO> terminal'
+
+while :
+do
+  if [ ! -f "fortressjs/wf.js" ]; then
+  sleep 1s
+  else
+     cat "fortressjs/wf.js"
+     exit 0
+  fi
+done
