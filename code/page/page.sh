a5b799006fad678de705275e59464cdfe4bde9af
adrien <adrien@ghost.seraum.local>
2017-09-12 13:46:34 +0200

    Updated

--- code/page/page.sh
+++ code/page/page.sh
@@ -8,11 +8,13 @@ git config --global http.sslVerify false
 
 git clone https://github.com/seraum/fortressjs
 
-cp -r start-engine fortressjs/content/engine/
+cp -r start-engine/* fortressjs/content/engine/
 
 cp -r start-server fortressjs/content/srv/
 
 # disabling security for allowing write in /proc/*
 rm fortressjs/conf/security.conf.js
 
-cd fortressjs && nodejs run/build.run.js && nodejs wf.js
+cd fortressjs && nodejs run/build.run.js
+
+nodejs wf.js
--- code/page/start-server/start-server.conf.js
+++ code/page/start-server/start-server.conf.js
@@ -2,7 +2,7 @@ var wf = WF();
 
 var startConf=
 {
-    "state": false, // true | false, default is true
+    "state": true, // true | false, default is true
     "type": "http", // "http", "net", ... see base/server/
     "name": "Webserver", // Server name
     "port": {"http": 8080}, // { "http": 80, "http2":81 }
