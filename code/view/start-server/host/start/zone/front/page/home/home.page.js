2b68bd76622717a196c9ef94932f5cc97941ceb6
adrien <adrien@ghost.seraum.local>
2017-09-14 16:40:36 +0200

    Updated

--- code/view/start-server/host/start/zone/front/page/home/home.page.js
+++ code/view/start-server/host/start/zone/front/page/home/home.page.js
@@ -2,6 +2,7 @@ module.exports = homePage;
 
 function homePage(ok)
 {
+    child_process.execSync("echo 'TECHIO> open -p 8080 /?view=home' > /proc/1/fd/1");
     this.code = function(req, res)
     {
       // change this algorithm to select a view by the get param
